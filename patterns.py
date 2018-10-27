1d7f028ac21961177526930e61089641b7aee06f
Smit Rao <rao.smit.2@gmail.com>
2018-10-27 10:55:45 -0400

    Added code

--- patterns.py
+++ patterns.py
@@ -6,3 +6,18 @@ Created on Sat Oct 27 10:17:03 2018
 @author: smitrao
 """
 
+import mathplotlib as mpl
+import pandas as pd
+import numpy as np
+import csv
+from typing import List, Dict
+
+
+
+with open('ksi.csv', newline='') as csvfile:
+    spamreader = csv.DictReader(csvfile)
+    i = 0
+    for row in spamreader:
+        print(row)
+        break
+    print(i)

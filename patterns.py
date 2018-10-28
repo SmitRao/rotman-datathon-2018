eb942f2e5906c8181bf86084eb3dc9d6e56e46e9
Smit Rao <rao.smit.2@gmail.com>
2018-10-27 21:15:43 -0400

    Final updates

--- patterns.py
+++ patterns.py
@@ -13,12 +13,37 @@ from typing import List, Dict
 
 
 
+'''Check frequency of accidents in the year (for example, do most accidents tend to occur in the month of January)?'''
+
+
+YEAR_DICT = {}
+
 with open('ksi.csv', newline='') as csvfile:
-    spamreader = csv.DictReader(csvfile)
-    i = 0
-    for row in spamreader:
-        if i > 3:
-            break
-        print(row)
-        i+=1
-    
+    eventreader = csv.DictReader(csvfile)
+    for row in eventreader:
+        if row['YEAR'] not in YEAR_DICT:
+            YEAR_DICT.setdefault(row['YEAR'], [row['DATE']])
+        else:
+            YEAR_DICT[row['YEAR']] += [row['DATE']]
+
+YEARS = list(YEAR_DICT.keys())
+YEARS.sort() # displays years from 2007 to 2017
+
+FREQ_DICT = {}
+
+for year in YEAR_DICT:
+    FREQ_DICT[int(year)] = []
+    for date in YEAR_DICT[year]:
+        FREQ_DICT[int(year)].append(int(date[5:7]))
+
+for year in FREQ_DICT:
+
+    FREQ_DICT[year].append({1:FREQ_DICT[year].count(1), 2:FREQ_DICT[year].count(2), \
+                            3:FREQ_DICT[year].count(3), 4:FREQ_DICT[year].count(4), \
+                            5:FREQ_DICT[year].count(5), 6:FREQ_DICT[year].count(6), \
+                            7:FREQ_DICT[year].count(7), 8:FREQ_DICT[year].count(8), \
+                            9:FREQ_DICT[year].count(9), 10:FREQ_DICT[year].count(10), \
+                            11:FREQ_DICT[year].count(11), 12:FREQ_DICT[year].count(12)})
+
+for y in FREQ_DICT:
+    print(FREQ_DICT[y][-1])
